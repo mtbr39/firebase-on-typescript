051ce8630265300fadcf3fe27e00a6da106ebd10
mtbr39 <nm1552jp@gmail.com>
2023-08-11 01:52:41 +0900

    comment

--- src/firebase/firebase_controll.tsx
+++ src/firebase/firebase_controll.tsx
@@ -1,6 +1,6 @@
 
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+// import { getAnalytics } from "firebase/analytics";
 import { getDatabase, ref, set } from "firebase/database";
 
 const firebaseConfig = {
@@ -15,7 +15,7 @@ const firebaseConfig = {
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
 // const database = getDatabase(app);
 
