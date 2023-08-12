414eee5e46bdc7e0a1ee36cf8ac6030549d52919
mtbr39 <nm1552jp@gmail.com>
2023-08-13 03:52:16 +0900

    Canvas Component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
 import './App.css'
 import {writeUserData} from './firebase/firebase_controll.tsx'
+import Canvas from './components/Canvas.tsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+      {/* 
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -16,7 +18,8 @@ function App() {
         <a href="https://react.dev" target="_blank">
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
-      </div>
+      </div> 
+      */}
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -29,9 +32,7 @@ function App() {
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Canvas />
     </>
   )
 }
---
+++ src/components/Canvas.tsx
@@ -0,0 +1,36 @@
+import { useState, useEffect } from 'react'
+
+const Canvas = () => {
+    // contextを状態として持つ
+    const [context,setContext] = useState<CanvasRenderingContext2D|null>(null)
+    // 画像読み込み完了トリガー
+    // const [loaded,setLoaded] = useState(false)
+    // onMountでcontextを取る
+    useEffect(()=>{
+        const canvas: HTMLCanvasElement = document.getElementById("canvas") as HTMLCanvasElement
+        const canvasContext = canvas.getContext("2d")
+        setContext(canvasContext)
+    },[])
+    // context登録後
+    useEffect(()=>{
+        if(context!==null)
+        {
+            // const img = new Image()
+            // img.src = "img.jpg" // 描画する画像など
+            // img.onload = () => {
+            //     context.drawImage(img,0,0)
+            //     // 更にこれに続いて何か処理をしたい場合
+            //     setLoaded(true)
+            // }
+        }
+    },[context])
+    // useEffect(()=>{
+    //     if(loaded)
+    //     {
+    //         // それに続く処理
+    //     }
+    // },[loaded])
+    return <canvas width="1280" height="720" id="canvas"></canvas>
+}
+
+export default Canvas
