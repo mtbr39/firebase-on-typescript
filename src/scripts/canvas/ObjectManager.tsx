d77a4dc979fb4fad58e6cd94433b07351c599b3f
mtbr39 <nm1552jp@gmail.com>
2023-08-13 04:28:38 +0900

    ObjectManager

---
+++ src/scripts/canvas/ObjectManager.tsx
@@ -0,0 +1,14 @@
+
+class ObjectManager {
+    x: number;
+    y: number;
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+    add() {
+        
+    }
+}
+
+export default ObjectManager
